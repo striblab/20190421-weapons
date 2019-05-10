fb6f615fd42620f7e1e4661e76b42de7c1514d2f
Frey Hargarten <harga012@umn.edu>
2019-05-10 14:23:10 -0500

    chart tweak

--- app/chart.js
+++ app/chart.js
@@ -15,7 +15,7 @@ class Chart {
             top: 20,
             right: 40,
             bottom: 20,
-            left: 120,
+            left: 60,
         };
 
         self.chartCounts = c3.generate({
